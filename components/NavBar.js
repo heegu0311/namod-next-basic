0db16610ec22178b40cde1c181261e6a35b71957
heegu0311 <heegu0311@gmail.com>
2023-01-29 11:32:24 +0900

    lecture 1.4 : use CSS module in next js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,30 @@
 import Link from "next/link";
+import styles from "./NavBar.module.css";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+  const router = useRouter();
+
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+      <Link
+        className={[
+          styles.link,
+          router.pathname === "/" ? styles.active : "",
+        ].join(" ")}
+        href="/"
+      >
+        Home
+      </Link>
+      <Link
+        className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : "",
+        ].join(" ")}
+        href="/about"
+      >
+        About
+      </Link>
     </nav>
   );
 }
