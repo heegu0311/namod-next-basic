cdd0cc42f1e92b1165213ce30b8e040f993f114b
heegu0311 <heegu0311@gmail.com>
2023-02-14 00:27:49 +0900

    lecture 2.7 : SSR with router params

--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -1,8 +1,19 @@
-import { useRouter } from "next/router";
+import Seo from "@/components/Seo";
 
-export default function Detail() {
-  const router = useRouter();
-  console.log(router.query);
-  const [title, id] = router.query.params;
-  return <h4>{title}</h4>;
+export default function Detail({ params }) {
+  const [title, id] = params || [];
+
+  return (
+    <div>
+      <Seo title={title} /> <h4>{title}</h4>
+    </div>
+  );
+}
+
+export function getServerSideProps({ params: { params } }) {
+  return {
+    props: {
+      params,
+    },
+  };
 }
